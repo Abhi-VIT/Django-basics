8413c372ebba23a81db8b562bac65ae758048e3b
Abhi-VIT <abhishekkumar10042001@gmail.com>
2025-10-23 23:39:20 +0530

    admin - Delete members

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 A basic of Django with and simple project in it.
 Learning the Django for web Framework
 - Date- 23 october 2025 **Display Data** *Prep template and view*
+- Date- 23 October 2025 **Admin** *delete Members*
--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-imion^vjc8474rwv-vg3hc+7@mp=)-&^sj@b9+^xu=+)zzu!^*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
